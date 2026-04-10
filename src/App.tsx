49dd5052e56f8122f7874d24360743b4db89cdf6
wallaceespy <wallace17.esp@gmail.com>
2026-04-09 22:54:36 -0300

    ✨ New Feature:  Postagem e Perfil

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,13 @@ import Home from "./pages/home/Home"
 import Cadastro from "./pages/cadastro/Cadastro"
 import Login from "./pages/login/Login"
 import { AuthProvider } from "./contexts/AuthContext"
-import DeletarTema from "./components/tema/deletartema/deletarTema"
+import DeletarTema from "./components/tema/deletartema/DeletarTema"
 import FormTema from "./components/tema/formtema/FormTema"
 import ListaTemas from "./components/tema/listatemas/ListaTemas"
+import ListaPostagens from "./components/postagem/listapostagens/ListaPostagens"
+import FormPostagem from "./components/postagem/formpostagem/FormPostagem"
+import DeletarPostagem from "./components/postagem/deletarpostagem/DeletarPostagem"
+import Perfil from "./pages/perfil/Perfil"
 
 
 function App() {
@@ -27,6 +31,11 @@ function App() {
 							<Route path="/cadastrartema" element={<FormTema />} />
 							<Route path="/editartema/:id" element={<FormTema />} />
 							<Route path="/deletartema/:id" element={<DeletarTema />} />
+              <Route path="/postagens" element={<ListaPostagens />} />
+              <Route path="/cadastrarpostagem" element={<FormPostagem />} />
+              <Route path="/editarpostagem/:id" element={<FormPostagem />} />
+              <Route path="/deletarpostagem/:id" element={<DeletarPostagem />} />
+              <Route path="/perfil" element={<Perfil />} />
             </Routes>
           </div>
           <Footer />
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,3 +1,6 @@
+import ListaPostagens from "../../components/postagem/listapostagens/ListaPostagens"
+import ModalPostagem from "../../components/postagem/modalpostagem/ModalPostagem"
+
 function Home() {
     return (
         <>
@@ -21,15 +24,14 @@ function Home() {
                         </p>
 
                         <div className="flex justify-around gap-4">
-                            <div className='rounded text-white 
-                                            border-white border-solid border-2 py-2 px-4'>
-                                Nova Postagem
-                            </div>
+                             <ModalPostagem />
                         </div>
                     </div>
 
                 </div>
             </div>
+
+             <ListaPostagens />
         </>
     )
 }
